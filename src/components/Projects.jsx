3e07226991a1c3b969e0742bae6faaf33edd070e
Mikkel Ofrim <39702459+Mikkelof@users.noreply.github.com>
2022-07-13 13:59:00 +0200

    Added skeleton for projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
 
+import onion from '../assets/Onion.png'
+import sprint from '../assets/Sprint.png'
+
 import FaGithub from 'react-icons/fa'
 
 const Projects = () => {
   return (
     <div className='w-full'>
-        <div className='w-full h-full bg-slate-900 absolute mix-blend-overlay'></div>
-        <div className='max-w-[1240px] mx-auto py-12'>
+
+        //TODO: Adjust height (h-[x]) to fit the page after adding all projects
+
+        <div className='w-full h-[1500px] bg-slate-900 absolute mix-blend-overlay'></div>
+        <div className='max-w-[1500px] mx-auto py-12'>
             <div className='text-center py-8 text-slate-300'>
                 <h2 className='text-5xl font-bold'>Projects</h2>
+            </div>
+            <div className='wrap w-5/6 mx-auto relative'>
+                <div className='grid grid-cols-10 py-12'>
+                    <div className='row-span-full col-start-1 col-span-8 self-center'>
+                        <img src={onion} alt="/" />
+                    </div>
+                    <div className='row-span-full col-span-6 col-end-11 self-center
+                    bg-white text-slate-900 m-4 p-4 rounded-xl shadow-2xl'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est unde vero, eum sint, repudiandae inventore doloribus consectetur deserunt similique autem porro. Quaerat dolore in officiis laborum maiores id tenetur. In id aliquam obcaecati consequatur quis non totam sint, quasi, qui est nihil maxime iste aliquid incidunt at earum quod nesciunt.
+                    </div>
+                </div>
+                <div className='grid grid-cols-10 py-12'>
+                    <div className='row-span-full col-end-11 col-span-8 self-center'>
+                        <img src={sprint} alt="/" />
+                    </div>
+                    <div className='row-span-full col-span-6 col-start-1 self-center
+                    bg-white text-slate-900 m-4 p-4 rounded-xl shadow-2xl'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est unde vero, eum sint, repudiandae inventore doloribus consectetur deserunt similique autem porro. Quaerat dolore in officiis laborum maiores id tenetur. In id aliquam obcaecati consequatur quis non totam sint, quasi, qui est nihil maxime iste aliquid incidunt at earum quod nesciunt.
+                    </div>
+                </div>
             </div>
         </div>
     </div>
