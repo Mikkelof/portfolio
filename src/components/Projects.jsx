127b1a16a13ebb58bee9663631b0a66b4fdf5b5e
Mikkel Ofrim <39702459+Mikkelof@users.noreply.github.com>
2023-08-16 20:36:35 +0200

    Updated projects and button layout

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,10 +7,23 @@ import booking from '../assets/Booking.jpg'
 import drikkelek from '../assets/Drikkelek.jpg'
 import blodsukkerkalk from '../assets/Blodsukkerkalk.png'
 import todolist from '../assets/Todolist.png'
+import nda from '../assets/nda.png'
+import minside from '../assets/Minside.png'
 
 import {FaGithub, FaPlay} from 'react-icons/fa'
 
 const Projects = () => {
+    const Button = ({ href, label, icon }) => (
+        <a
+          href={href}
+          rel="noopener noreferrer"
+          target="_blank"
+          className='flex items-center bg-slate-900 border border-slate-900 hover:bg-transparent hover:text-slate-900 
+          rounded-md text-white px-4 py-2 my-2'
+        >
+          {icon} <span className='text-sm'>{label}</span>
+        </a>
+      );
   return (
     <div className='w-full'>
         <div className='mx-auto py-12 bg-slate-900'>
@@ -18,6 +31,41 @@ const Projects = () => {
                 <h2 className='text-5xl font-bold'>Projects</h2>
             </div>
             <div className='wrap w-5/6 mx-auto max-w-[1500px]'>
+            <div className='grid grid-cols-10 py-12'>
+                    <div className='row-span-full col-end-11 col-span-8 self-center'>
+                            <img src={minside} alt="/" />
+                        </div>
+                        <div className='row-span-full col-span-4 col-start-1 self-center
+                        bg-white text-slate-900 m-4 p-4 rounded-xl shadow-2xl border border-slate-900'>
+                            <p className='text-4xl py-3 flex text-center'>Skatteetaten Min Side<span className='text-lg text-slate-500 flex flex-col justify-end px-2'>07.2023</span></p>
+                            <p className='py-2'>During the summer of 2023 I spent eight very educational weeks working at Skatteetaten in Oslo. Here I worked as a backend developer on a project on min side. Here I used Spring Boot, SQL along with various other technologies</p>
+                        </div>
+                </div>
+                <div className='grid grid-cols-10 py-12'>
+                    <div className='row-span-full col-start-1 col-span-8 self-center'>
+                        <img src={nda} alt="/" />
+                    </div>
+                    <div className='grid grid-cols-10 py-12'>
+                </div>
+                    <div className='row-span-full col-span-4 col-end-11 self-center items-center
+                    bg-white text-slate-900 m-4 p-4 rounded-xl shadow-2xl border border-slate-900'>
+                        <p className='text-4xl py-3 flex text-center'>NDA<span className='text-lg text-slate-500 flex flex-col justify-end px-2'>05.2023</span></p>
+                        <p className='py-2'>The bachelor's thesis for me and a classmate was creating a system for generating, sending, storing and displaying non-disclosure agreements for all the volunteers in NTNUI. This was made using Node.js, React and MongoDB to mention the major technologies.</p>
+                        <div className='flex mt-4'>
+                            <Button
+                            href='https://github.com/NTNUI/nda'
+                            label='View on GitHub'
+                            icon={<FaGithub className='h-6 mr-2' />}
+                            />
+                            <span className='ml-4'></span>
+                            <Button
+                            href='https://www.youtube.com/watch?v=h_fH1oFMYqs'
+                            label='View a Demo'
+                            icon={<FaPlay className='h-6 mr-2' />}
+                            />
+                        </div>
+                    </div>
+                </div>
                 <div className='grid grid-cols-10 py-12'>
                     <div className='row-span-full col-end-11 col-span-8 self-center'>
                         <img src={todolist} alt="/" />
@@ -27,8 +75,11 @@ const Projects = () => {
                         <p className='text-4xl py-3 flex text-center'>Todo list app<span className='text-lg text-slate-500 flex flex-col justify-end px-2'>10.2022</span></p>
                         <p className='py-2'>This was a project in the subject IDATT2506 at NTNU. The task was to create a cross platform to-do list application. I made this using React Native. Each
                         to-do list has zero or many tasks. Each task can be marked as done or ongoing. All the data is stored locally on the device and will be saved when restaring the app/device.</p>
-                        <a href='https://github.com/Mikkelof/IDATT2506-App-Project' rel="noopener noreferrer" target="_blank" className='uppercase flex bg-slate-900 border border-slate-900 hover:bg-transparent hover:text-slate-900 
-                        rounded-md text-white p-2 drop-shadow-md max-w-[168px]'><FaGithub className='h-6' /> <span className='px-1'>View on github</span></a>
+                        <Button
+                            href='https://github.com/Mikkelof/IDATT2506-App-Project'
+                            label='View on GitHub'
+                            icon={<FaGithub className='h-6 mr-2' />}
+                        />
                     </div>
                 </div>
                 <div className='grid grid-cols-10 py-12'>
@@ -39,8 +90,11 @@ const Projects = () => {
                     bg-white text-slate-900 m-4 p-4 rounded-xl shadow-2xl border border-slate-900'>
                         <p className='text-4xl py-3 flex text-center'>Portfolio<span className='text-lg text-slate-500 flex flex-col justify-end px-2'>07.2022</span></p>
                         <p className='py-2'>A portfolio site I created to showcase some of my earlier projects. It was made using React.js and Tailwind CSS.</p>
-                        <a href='https://github.com/Mikkelof/portfolio' rel="noopener noreferrer" target="_blank" className='uppercase flex bg-slate-900 border border-slate-900 hover:bg-transparent hover:text-slate-900 
-                        rounded-md text-white p-2 drop-shadow-md max-w-[168px]'><FaGithub className='h-6' /> <span className='px-1'>View on github</span></a>
+                        <Button
+                            href='https://github.com/Mikkelof/portfolio'
+                            label='View on GitHub'
+                            icon={<FaGithub className='h-6 mr-2' />}
+                        />
                     </div>
                 </div>
                 <div className='grid grid-cols-10 py-12'>
@@ -52,8 +106,11 @@ const Projects = () => {
                         <p className='text-4xl py-3 flex text-center'>Onion Routing<span className='text-lg text-slate-500 flex flex-col justify-end px-2'>04.2022</span></p>
                         <p className='py-2'>This was a voluntary assignment in the course IDATT2104. The task was to create an onion routing service. My implementation was made using Node.js
                         with a simple client-side written in Vue.js. Full description of what is is can be read on the README-file on GitHub.</p>
-                        <a href='https://github.com/Mikkelof/Onion-routing' rel="noopener noreferrer" target="_blank" className='uppercase flex bg-slate-900 border border-slate-900 hover:bg-transparent hover:text-slate-900 
-                        rounded-md text-white p-2 drop-shadow-md max-w-[168px]'><FaGithub className='h-6' /> <span className='px-1'>View on github</span></a>
+                        <Button
+                            href='https://github.com/Mikkelof/Onion-routing'
+                            label='View on GitHub'
+                            icon={<FaGithub className='h-6 mr-2' />}
+                        />
                     </div>
                 </div>
                 <div className='grid grid-cols-10 py-12'>
@@ -76,8 +133,11 @@ const Projects = () => {
                         <p className='text-4xl py-3 flex text-center'>Booking Script<span className='text-lg text-slate-500 flex flex-col justify-end px-2'>02.2021</span></p>
                         <p className='py-2'>When the SiT gyms reopened after lockdown you had to book time for each workout two days in advance. However, these sessions were usually fully booked within 
                         seconds and you weren't always able to sit ready when they opened. To combat this I created a python script that got me the sessions I wanted without all the stress.</p>
-                        <a href='https://github.com/Mikkelof/SitBookingScript' rel="noopener noreferrer" target="_blank" className='uppercase flex bg-slate-900 border border-slate-900 hover:bg-transparent hover:text-slate-900 
-                        rounded-md text-white p-2 drop-shadow-md max-w-[168px]'><FaGithub className='h-6' /> <span className='px-1'>View on github</span></a>
+                        <Button
+                            href='https://github.com/Mikkelof/SitBookingScript'
+                            label='View on GitHub'
+                            icon={<FaGithub className='h-6 mr-2' />}
+                        />
                     </div>
                 </div>
                 <div className='grid grid-cols-10 py-12'>
@@ -92,10 +152,19 @@ const Projects = () => {
                         ticking down, you are supposed to click the button when the green LED is lit. If you miss, a random interval of time is subtracted from the time and you have to take 
                         a sip. If you click the button while the green LED is lit, you can pass the "bomb" to the next person. Whoever has the "bomb" when it goes off has to chug their drink. 
                         It was made using a Raspberry Pi and python.</p>
-                        <span className='flex'><a href='https://github.com/Mikkelof/BlinkGame' rel="noopener noreferrer" target="_blank" className='uppercase flex bg-slate-900 border border-slate-900 hover:bg-transparent hover:text-slate-900 
-                        rounded-md text-white p-2 drop-shadow-md max-w-[168px] py-2'><FaGithub className='h-6' /> <span className='px-1'>View on github</span></a>
-                        <a href='https://www.youtube.com/watch?v=yVyEQ5xTnZs' rel="noopener noreferrer" target="_blank" className='uppercase flex bg-slate-900 border border-slate-900 hover:bg-transparent hover:text-slate-900 
-                        rounded-md text-white p-2 drop-shadow-md max-w-[168px] py-2'><FaPlay className='h-6' /> <span className='px-1'>View a demo</span></a></span>
+                        <div className='flex mt-4'>
+                            <Button
+                            href='https://github.com/Mikkelof/BlinkGame'
+                            label='View on GitHub'
+                            icon={<FaGithub className='h-6 mr-2' />}
+                            />
+                            <span className='ml-4'></span>
+                            <Button
+                            href='https://www.youtube.com/watch?v=yVyEQ5xTnZs'
+                            label='View a Demo'
+                            icon={<FaPlay className='h-6 mr-2' />}
+                            />
+                        </div>
                     </div>
                 </div>
                 <div className='grid grid-cols-10 py-12'>
@@ -108,8 +177,11 @@ const Projects = () => {
                         <p className='py-2'>This was the first coding project I ever did and is the reason I got into programming. It is a simple insulin calculator made for personal use 
                         as I could not find any calculators fitting my needs on the Google Play Store. At the time of writing it has almost 25,000 downloads and more than 4.1/5 stars 
                         from almost 50 ratings.</p>
-                        <a href='https://github.com/Mikkelof/InsulinCalc' rel="noopener noreferrer" target="_blank" className='uppercase flex bg-slate-900 border border-slate-900 hover:bg-transparent hover:text-slate-900 
-                        rounded-md text-white p-2 drop-shadow-md max-w-[168px]'><FaGithub className='h-6' /> <span className='px-1'>View on github</span></a>
+                        <Button
+                            href='https://github.com/Mikkelof/InsulinCalc'
+                            label='View on GitHub'
+                            icon={<FaGithub className='h-6 mr-2' />}
+                        />
                     </div>
                 </div>
             </div>
